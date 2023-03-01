f1b276dd080fb6a970837eb3b90dfddb109c5004
Matheus S. Oliveira <matheuskiller6@gmail.com>
2023-03-01 17:08:17 -0300

    Projeto PDP - React com os componentes criados

---
+++ src/paginas/cadastro/Cadastro.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Cadastro.css'
+
+function Cadastro(){
+    return(
+        <>
+        <h1>Tela de cadastro</h1>
+        </>
+    );
+}
+
+export default Cadastro;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Home.css'
+function Home(){
+    return(
+        <h1>Home Page</h1>
+    );
+}
+
+export default Home;
